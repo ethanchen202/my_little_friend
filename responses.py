3b9e086b1c3779dbba4d16184529f73413ee0556
Ethan Chen <ethan.chwy@gmail.com>
2024-12-05 11:57:24 -0800

    migrated cypherbot to gpt-4.0-mini

--- responses.py
+++ responses.py
@@ -15,24 +15,45 @@ def get_acc_info(riot_ign: str, tag: str):
     url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{riot_ign}/{tag}"
     headers = {"X-Riot-Token": RIOT_GAMES_API_KEY}
     response = requests.get(url, headers=headers)
+    # print(f'acc info: {response.json()}')
+    if response.status_code != 200:
+        return None
     return response.json()
 
 def get_summoner_info(puuid: str):
     url = f"https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}"
     headers = {"X-Riot-Token": RIOT_GAMES_API_KEY}
     response = requests.get(url, headers=headers)
+    # print(f'summoner info: {response.json()}')
+    if response.status_code != 200:
+        return None
     return response.json()
 
 def get_lol_info(summoner_id: str):
     url = f"https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/{summoner_id}"
     headers = {"X-Riot-Token": RIOT_GAMES_API_KEY}
     response = requests.get(url, headers=headers)
+    # print(f'lol info: {response.json()}')
+    # print(response.status_code)
+    if response.status_code != 200:
+        return None
     return response.json()
 
 def display_lol_info(riot_ign: str, tag: str) -> str:
     acc_info = get_acc_info(riot_ign, tag)
+
+    if acc_info == None:
+        return "Seems like I don't have access to that data."
+
     summoner_info = get_summoner_info(acc_info["puuid"])
+
+    if summoner_info == None:
+        return "Seems like I don't have access to that data."
+    
     lol_info = get_lol_info(summoner_info["id"])
+
+    if lol_info == None:
+        return "Seems like I don't have access to that data."
 
     if lol_info == []:
         return "No data was found"
@@ -52,14 +73,14 @@ def get_response(user_input: str) -> str:
         messages=[
             {
                 "role": "system",
-                "content": "You are roleplaying as Cypher from Valorant"
+                "content": "You are roleplaying as Cypher from Valorant."
             },
             {
                 "role": "user",
                 "content": user_input,
             },
         ],
-        model="gpt-3.5-turbo",
+        model="gpt-4o-mini",
     )
 
     return chat_completion.choices[0].message.content
